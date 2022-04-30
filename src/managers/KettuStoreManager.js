943d0606155d06cf0124572f5dd103da1866d538
eeehh <lachlan.walls1@gmail.com>
2022-04-30 10:34:46 +1000

    feat(KettuStoreManager): add #patch

--- src/managers/KettuStoreManager.js
+++ src/managers/KettuStoreManager.js
@@ -14,22 +14,34 @@ class KettuStoreManager {
   }
 
   /**
-   * Writes data to the data store
-   * @param {string} key Key to write data to
-   * @param {*} data JSON data to write
-   * @returns {Promise}
-   */
-  set (key, data) {
-    return this.client.api.users('@me').store(key).post({ data: { data } })
-  }
-
-  /**
    * Retrieves data from the data store
    * @param {string} key Key to retrieve data from
    * @returns {Promise<*>}
    */
   fetch (key) {
     return this.client.api.users('@me').store(key).get()
+  }
+
+  /**
+   * Writes data to the data store (like an overwrite)
+   * @param {string} key Key to write data to
+   * @param {*} data JSON data to write
+   * @returns {Promise<*>}
+   */
+  async set (key, data) {
+    const row = await this.client.api.users('@me').store(key).post({ data: { data } })
+    return row.data
+  }
+
+  /**
+   * Updates data in the data store (like a patch), must be on an object
+   * @param {string} key Key to patch data to
+   * @param {object} data JSON data to patch
+   * @returns {Promise<*>}
+   */
+  async patch (key, data) {
+    const row = await this.client.api.users('@me').store(key).patch({ data: { data } })
+    return row.data
   }
 
   /**
